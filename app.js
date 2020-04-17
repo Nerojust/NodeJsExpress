a8b7432e115a5f06574e479477c896ee524e4f9d
Nyerhovwo Adjekughene <nerojust4@gmail.com>
2020-04-18 00:20:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const Book = require("./models/bookModel");
+const bookRouter = require("./routes/bookRouter")(Book);
+const port = process.env.PORT || 3000;
+
+const mongoose = require("mongoose");
+const db = mongoose.connect(
+  "mongodb://localhost:27017/bookAPI",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    if (err) {
+      console.log(`the error is ${err}`);
+    }
+  }
+);
+
+app.use("/api", bookRouter);
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
