a27431be3b38fc0ab182adb9c11d046dbc10b240
Nyerhovwo Adjekughene <nerojust4@gmail.com>
2020-04-18 21:52:29 +0100

    edited repoonse function

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,7 @@
 module.exports= function setGeneralResponse(input) {
     return {
       code: 200,
-      message: "successful",
+      message: "Request successful",
       recordCount: input.length,
       result: input
     };
