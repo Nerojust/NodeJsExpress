3d97b2adf25e8042644f2c0098e692aef5d172d3
Nyerhovwo Adjekughene <nerojust4@gmail.com>
2020-04-19 10:09:18 +0100

    added pm2

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,7 @@
 module.exports= function setGeneralResponse(input) {
     return {
       code: 200,
-      message: "Request successful",
+      message: "Request successful again",
       recordCount: input.length,
       result: input
     };
